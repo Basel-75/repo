07ad2e2bd93fb98a5b55ecb1a6db6b657c0c2448
Basel <basel@gmail.com>
2024-07-31 10:52:56 +0300

    first commit

---
+++ bin/dart_and_git.dart
@@ -0,0 +1,5 @@
+import 'package:dart_and_git/dart_and_git.dart' as dart_and_git;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_and_git.calculate()}!');
+}
---
+++ test/dart_and_git_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_and_git/dart_and_git.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
