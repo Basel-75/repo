2453724199d495316d427feddd32cdebdf1a373c
Basel-75 <basel_1422@outlook.com>
2024-07-31 11:33:00 +0300

    secound commit

--- bin/dart_and_git.dart
+++ bin/dart_and_git.dart
@@ -1,5 +1,10 @@
 import 'package:dart_and_git/dart_and_git.dart' as dart_and_git;
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_and_git.calculate()}!');
+ printNum();
+}
+printNum(){
+  for(int i=0; i<=10;i++){
+    print(i);
+  }
 }
