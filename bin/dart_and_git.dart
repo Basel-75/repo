9155ab5a1164626e25fadc1c2212ba3ec2d88046
Basel-75 <basel_1422@outlook.com>
2024-07-31 11:41:51 +0300

    create desplay user id function

--- bin/dart_and_git.dart
+++ bin/dart_and_git.dart
@@ -2,9 +2,15 @@ import 'package:dart_and_git/dart_and_git.dart' as dart_and_git;
 
 void main(List<String> arguments) {
  printNum();
+ printUser();
 }
 printNum(){
   for(int i=0; i<=10;i++){
     print(i);
+  }
+}
+printUser(){
+  for(int i=0; i<=10;i++){
+    print('user $i');
   }
 }
